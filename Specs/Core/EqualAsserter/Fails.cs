2f7339038ad3d52e11bde13f2b8e31fcc167addd
Bryce <brycekbargar@gmail.com>
2015-08-31 22:33:04 -0500

    Better organize Specs

--- Specs/Core/EqualAsserter/Fails.cs
+++ Specs/Core/EqualAsserter/Fails.cs
@@ -5,52 +5,8 @@ namespace Nilgri.Specs.Core
   using Nilgri.Core;
 
   using Subject = Nilgri.Core.EqualAsserter;
-  public class EqualAsserter
+  public partial class EqualAsserter
   {
-    public class Passes
-    {
-      private Subject _subject;
-
-      public Passes()
-      {
-        _subject = new Subject();
-      }
-
-      [Fact]
-      public void Int32()
-      {
-        var testValue = 1;
-        var testState = new AssertionState<Int32>(() => testValue);
-
-        var ex = Record.Exception(() => _subject.Assert(testState, testValue));
-
-        Assert.Null(ex);
-      }
-
-      [Fact]
-      public void String()
-      {
-        var testValue = @"I'm a string!";
-        var testState = new AssertionState<string>(() => testValue);
-
-        var ex = Record.Exception(() => _subject.Assert(testState, testValue));
-
-        Assert.Null(ex);
-
-      }
-
-      [Fact]
-      public void Object()
-      {
-        var testValue = new { I = "Have ", AtLeast = 3, Properties = true};
-        var testState = new AssertionState<object>(() => testValue);
-
-        var ex = Record.Exception(() => _subject.Assert(testState, testValue));
-
-        Assert.Null(ex);
-      }
-    }
-
     public class Fails
     {
       private Subject _subject;
