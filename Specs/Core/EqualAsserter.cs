31d21f6c4f26c74ff92b6c1e9844bfd58207e9eb
Bryce <brycekbargar@gmail.com>
2015-08-31 22:29:49 -0500

    RGR Pass/Fail Bryces commonly used types

--- Specs/Core/EqualAsserter.cs
+++ Specs/Core/EqualAsserter.cs
@@ -7,27 +7,90 @@ namespace Nilgri.Specs.Core
   using Subject = Nilgri.Core.EqualAsserter;
   public class EqualAsserter
   {
-    private Subject _subject;
-
-    public EqualAsserter()
+    public class Passes
     {
-      _subject = new Subject();
+      private Subject _subject;
+
+      public Passes()
+      {
+        _subject = new Subject();
+      }
+
+      [Fact]
+      public void Int32()
+      {
+        var testValue = 1;
+        var testState = new AssertionState<Int32>(() => testValue);
+
+        var ex = Record.Exception(() => _subject.Assert(testState, testValue));
+
+        Assert.Null(ex);
+      }
+
+      [Fact]
+      public void String()
+      {
+        var testValue = @"I'm a string!";
+        var testState = new AssertionState<string>(() => testValue);
+
+        var ex = Record.Exception(() => _subject.Assert(testState, testValue));
+
+        Assert.Null(ex);
+
+      }
+
+      [Fact]
+      public void Object()
+      {
+        var testValue = new { I = "Have ", AtLeast = 3, Properties = true};
+        var testState = new AssertionState<object>(() => testValue);
+
+        var ex = Record.Exception(() => _subject.Assert(testState, testValue));
+
+        Assert.Null(ex);
+      }
     }
 
-    [Fact]
-    public void Pass()
+    public class Fails
     {
-      var testState = new AssertionState<int>(() => 1);
+      private Subject _subject;
 
-      _subject.Assert(testState, 1);
-    }
+      public Fails()
+      {
+        _subject = new Subject();
+      }
 
-    [Fact]
-    public void Fail()
-    {
-      var testState = new AssertionState<int>(() => 1);
+      [Fact]
+      public void Int32()
+      {
+        var testState = new AssertionState<Int32>(() => 1);
 
-      Assert.Throws<Exception>(() => _subject.Assert(testState, 2));
+        var ex = Record.Exception(() => _subject.Assert(testState, 2));
+
+        Assert.NotNull(ex);
+      }
+
+      [Fact]
+      public void String()
+      {
+        var testState = new AssertionState<string>(() => @"I'm a string!");
+
+        var ex = Record.Exception(() => _subject.Assert(testState, @"I'm a different string!"));
+
+        Assert.NotNull(ex);
+
+      }
+
+      [Fact]
+      public void Object()
+      {
+        var testValue = new { I = "Have ", AtLeast = 3, Properties = true};
+        var testState = new AssertionState<object>(() => testValue);
+
+        var ex = Record.Exception(() => _subject.Assert(testState, new {}));
+
+        Assert.NotNull(ex);
+      }
     }
   }
 }
