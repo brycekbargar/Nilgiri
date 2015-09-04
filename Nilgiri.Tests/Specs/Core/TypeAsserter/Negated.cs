02df64883bd33ad80622ef01c7f00fe65dde0580
Bryce <brycekbargar@gmail.com>
2015-09-04 06:07:46 -0500

    RGR Assert types are not equal

---
+++ Nilgiri.Tests/Specs/Core/TypeAsserter/Negated.cs
@@ -0,0 +1,61 @@
+namespace Nilgiri.Specs.Core
+{
+  using System;
+  using Xunit;
+  using Nilgiri.Core;
+
+  using Subject = Nilgiri.Core.TypeAsserter;
+  public partial class TypeAsserter
+  {
+    public class Negated
+    {
+      private Subject _subject;
+
+      public Negated()
+      {
+        _subject = new Subject();
+      }
+
+      [Fact]
+      public void ValueTypes()
+      {
+        var testValue = 1;
+        var testState = new AssertionState<int>(() => testValue){ IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testState, typeof(bool)));
+        var exFail = Record.Exception(() => _subject.Assert(testState, typeof(int)));
+
+        Assert.Null(exPass);
+        Assert.NotNull(exFail);
+      }
+
+      private class StubClass { }
+      private class NotStubClass { }
+
+      [Fact]
+      public void ReferenceTypes()
+      {
+        var testValue = new StubClass();
+        var testState = new AssertionState<StubClass>(() => testValue){ IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testState, typeof(NotStubClass)));
+        var exFail = Record.Exception(() => _subject.Assert(testState, typeof(StubClass)));
+
+        Assert.Null(exPass);
+        Assert.NotNull(exFail);
+      }
+
+      [Fact]
+      public void Null()
+      {
+        var testState = new AssertionState<StubClass>(() => null){ IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testState, typeof(NotStubClass)));
+        var exFail = Record.Exception(() => _subject.Assert(testState, typeof(StubClass)));
+
+        Assert.Null(exPass);
+        Assert.NotNull(exFail);
+      }
+    }
+  }
+}
