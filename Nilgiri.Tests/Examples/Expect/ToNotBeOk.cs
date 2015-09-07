7a813f952c8f0c50ad05cc18b1ed3dca13b3d044
Bryce <brycekbargar@gmail.com>
2015-09-07 15:04:51 -0500

    RGR Integrate the Expect.To.Not.Be.Ok chain

---
+++ Nilgiri.Tests/Examples/Expect/ToNotBeOk.cs
@@ -0,0 +1,46 @@
+namespace Nilgiri.Examples
+{
+  using Xunit;
+  using Nilgiri.Tests.Common;
+  using static Nilgiri.ExpectStyle;
+
+  public partial class ExampleOf_Expect
+  {
+    public class To_Not_Be_Ok
+    {
+      [Fact]
+      public void Int32()
+      {
+        Expect(0).To.Not.Be.Ok();
+        Expect(() => 0).To.Not.Be.Ok();
+      }
+
+      [Fact]
+      public void Nullable()
+      {
+        Expect((bool?)false).To.Not.Be.Ok();
+        Expect(() => (bool?)false).To.Not.Be.Ok();
+      }
+
+      [Fact]
+      public void String()
+      {
+        Expect((string)null).To.Not.Be.Ok();
+        Expect(() => (string)null).To.Not.Be.Ok();
+        Expect(System.String.Empty).To.Not.Be.Ok();
+        Expect(() => System.String.Empty).To.Not.Be.Ok();
+        Expect("").To.Not.Be.Ok();
+        Expect(() => "").To.Not.Be.Ok();
+        Expect("   ").To.Not.Be.Ok();
+        Expect(() => "   ").To.Not.Be.Ok();
+      }
+
+      [Fact]
+      public void ReferenceTypes()
+      {
+        Expect((StubClass)null).To.Not.Be.Ok();
+        Expect(() => (StubClass)null).To.Not.Be.Ok();
+      }
+    }
+  }
+}
