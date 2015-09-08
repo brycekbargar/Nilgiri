6caa9d6eaed0fce344788182f5fe8c12cd367045
Bryce <brycekbargar@gmail.com>
2015-09-08 05:22:04 -0500

    RGR Test Nullables against null

--- Nilgiri.Tests/Specs/Core/Asserters/NullAsserter.cs
+++ Nilgiri.Tests/Specs/Core/Asserters/NullAsserter.cs
@@ -24,6 +24,52 @@ namespace Nilgiri.Specs.Core.Asserters
 
         Assert.NotNull(exFail);
       }
+
+      [Fact]
+      public void Nullables()
+      {
+        var testStatePass = new AssertionState<bool?>(() => (bool?)null);
+        var testStateFail = new AssertionState<bool?>(() => true);
+
+        var exPass = Record.Exception(() => _subject.Assert(testStatePass));
+        var exFail = Record.Exception(() => _subject.Assert(testStateFail));
+
+        Assert.Null(exPass);
+        Assert.NotNull(exFail);
+      }
+    }
+
+    public class Negated
+    {
+      private Subject _subject;
+
+      public Negated()
+      {
+        _subject = new Subject();
+      }
+
+      [Fact]
+      public void ValueTypes()
+      {
+        var testState = new AssertionState<int>(() => 1) { IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testState));
+
+        Assert.Null(exPass);
+      }
+
+      [Fact]
+      public void Nullables()
+      {
+        var testStatePass = new AssertionState<bool?>(() => true) { IsNegated = true };
+        var testStateFail = new AssertionState<bool?>(() => (bool?)null) { IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testStatePass));
+        var exFail = Record.Exception(() => _subject.Assert(testStateFail));
+
+        Assert.Null(exPass);
+        Assert.NotNull(exFail);
+      }
     }
   }
 }
