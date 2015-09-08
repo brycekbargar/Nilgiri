60a8a505062d9820eed0ead62d16a60a655d7bd9
Bryce <brycekbargar@gmail.com>
2015-09-07 22:13:46 -0500

    Refactor Asserter tests into a single file [ci skip]

--- Nilgiri.Tests/Specs/Core/Asserters/BooleanAsserter.cs
+++ Nilgiri.Tests/Specs/Core/Asserters/BooleanAsserter.cs
@@ -5,7 +5,7 @@ namespace Nilgiri.Specs.Core.Asserters
   using Nilgiri.Tests.Common;
 
   using Subject = Nilgiri.Core.Asserters.BooleanAsserter;
-  public partial class BooleanAsserter
+  public class BooleanAsserter
   {
     public class Normal
     {
@@ -47,6 +47,49 @@ namespace Nilgiri.Specs.Core.Asserters
         var exFail = Record.Exception(() => _subject.Assert(testState));
 
         Assert.NotNull(exFail);
+      }
+    }
+
+    public class Negated
+    {
+      private Subject _subject;
+
+      public Negated()
+      {
+        _subject = new Subject();
+      }
+
+      [Fact]
+      public void Boolean()
+      {
+        var testStatePass = new AssertionState<bool>(() => false) { IsNegated = true };
+        var testStateFail = new AssertionState<bool>(() => true) { IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testStatePass));
+        var exFail = Record.Exception(() => _subject.Assert(testStateFail));
+
+        Assert.Null(exPass);
+        Assert.NotNull(exFail);
+      }
+
+      [Fact]
+      public void ValueTypes()
+      {
+        var testState = new AssertionState<int>(() => 1) { IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testState));
+
+        Assert.Null(exPass);
+      }
+
+      [Fact]
+      public void ReferenceTypes()
+      {
+        var testState = new AssertionState<StubClass>(() => new StubClass()) { IsNegated = true };
+
+        var exPass = Record.Exception(() => _subject.Assert(testState));
+
+        Assert.Null(exPass);
       }
     }
   }
